0bad46db5f5fb416a539138b0dfadc8d6cf41d93
Amirul Akmal <amirul.akmal.putrajaya2003@gmail.com>
2021-09-23 11:18:04 +0800

    Players and Ball Moving

--- Pong/hea/box.h
+++ Pong/hea/box.h
@@ -1,23 +1,50 @@
 #include <SDL.h>
+#include <SDL_ttf.h>
 
+#include <time.h>
 #include <vector>
+
+#define ScreenWidth 1920
+#define ScreenHeight 1080
+constexpr auto BallSpeed = 2;
 
 class Box
 {	
+protected:
 	SDL_Surface* image = NULL;
 	SDL_Rect rectangle;
+	void check();
 public:
 
-	Box(int w, int h, uint32_t color);
+	Box(int x, int y,int w, int h,  uint32_t color);
 	void Draw(SDL_Surface* surface);
 	void Update(int h);
 };
-
 class GroupBox
 {
 	std::vector<Box*> groups;
 public:
-
 	void add(Box *box);
 	void update(SDL_Surface *surface, uint32_t color);
+};
+class Ball : public Box
+{
+	int moving = 0;
+	// False = ball start go toward left first
+	// True = ball start go toward right first
+	bool decision = false;
+	int deltaX = 0, deltaY = 0;
+	void LRDecision();
+	int random();
+public:
+	Ball(int x, int y, int w, int h, uint32_t color) : Box(x, y, w, h, color){};
+	void Init(const char filename[]);
+	void update();
+};
+class Text
+{
+	int left = 0;
+	int right = 0;
+public:
+
 };
--- Pong/src/box.cpp
+++ Pong/src/box.cpp
@@ -1,24 +1,44 @@
 #include <box.h>
 
 // For the Players
-Box::Box(int x, int y, uint32_t color)
+Box::Box(int x, int y, int w, int h, uint32_t color)
 {
-	image = SDL_CreateRGBSurface(0, 50, 200, 32, 0, 0, 0, 0);
+	image = SDL_CreateRGBSurface(0, w, h, 32, 0, 0, 0, 0);
 	rectangle.x = x;
 	rectangle.y = y;
-	rectangle.w = x + 50;
-	rectangle.h = y + 200;
+	rectangle.w = w;
+	rectangle.h = h;
 	SDL_FillRect(image, NULL, color);
 }
 void Box::Draw(SDL_Surface* surface)
 {
-	SDL_BlitSurface(image, NULL, surface, &rectangle);
+	try
+	{
+		SDL_BlitSurface(image, NULL, surface, &rectangle);
+	}
+	catch (const std::exception&)
+	{
+		SDL_Quit();
+	}
 }
-
+void Box::check()
+{
+	if (rectangle.y >= ScreenHeight - 210)
+	{
+		rectangle.y = ScreenHeight - 210;
+		rectangle.h = ScreenHeight - 210;
+	}
+	if (rectangle.y <= 2)
+	{
+		rectangle.y = 2;
+		rectangle.h = 2;
+	}
+}
 void Box::Update(int h)
 {
 	rectangle.y -= h;
 	rectangle.h -= h;
+	check();
 }
 
 // Players grouping
@@ -32,5 +52,77 @@ void GroupBox::update(SDL_Surface* surface, uint32_t color)
 	for (int i = 0 ; i < groups.size() ; i++)
 	{
 		groups[i]->Draw(surface);
+	}
+}
+
+// Ball 
+int Ball::random()
+{
+	srand(time(0));
+	return (rand() % 100);;
+}
+void Ball::Init(const char filename[])
+{
+	moving = random();
+	image = SDL_LoadBMP(filename);
+	if (image != NULL)
+	{
+		LRDecision();
+		printf("Image loaded successfully\n");
+	}
+	else
+	{
+		printf("Image failed to load to the memory\n");
+	}
+}
+void Ball::LRDecision()
+{
+	if (moving % 2 == 0)
+	{
+		decision = true;
+	}
+}
+void Ball::update()
+{
+	// Start Moving 
+	if (deltaX == 0 && deltaY == 0)
+	{
+		if (decision)
+		{
+			deltaX = BallSpeed;
+			deltaY = BallSpeed;
+		}
+		else
+		{
+			deltaX = -BallSpeed;
+			deltaY = -BallSpeed;
+		}
+	}
+
+	// the continous moving updation
+	rectangle.x += deltaX;
+	rectangle.y += deltaY;
+	rectangle.w += deltaX;
+	rectangle.h += deltaY;
+
+	// Check for players collision
+
+
+	// Check for Screen Border
+	if (rectangle.x < 5)
+	{
+		deltaX = BallSpeed;
+	}
+	if (rectangle.x > ScreenWidth - 45)
+	{
+		deltaX = -BallSpeed;
+	}
+	if (rectangle.y < 5)
+	{
+		deltaY = BallSpeed;
+	}
+	if (rectangle.y > ScreenHeight - 45)
+	{
+		deltaY = -BallSpeed;
 	}
 }
